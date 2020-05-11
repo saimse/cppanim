e77b0bdfb3515a4787b4b49f779f825f7f0c8dfd
Mile <vuletic.mile@jjzmaj.edu.rs>
2020-05-11 17:11:55 +0200

    Fixed file modes, added headers to master header

--- include/cppanim.hpp
+++ include/cppanim.hpp
@@ -1,7 +1,12 @@
 #ifndef CPPANIM
 #define CPPANIM
 
+#include <cppanim/animation.hpp>
+#include <cppanim/context.hpp>
+#include <cppanim/drawable.hpp>
+#include <cppanim/frame.hpp>
 #include <cppanim/fundamentals.hpp>
+#include <cppanim/screen.hpp>
 #include <cppanim/symbol.hpp>
 
 #endif
