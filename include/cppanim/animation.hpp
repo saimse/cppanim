6eb9d843227a85edbd05365237c499e24af0f1d9
Aleksa Mijatović <62738543+aleksamijat@users.noreply.github.com>
2020-05-13 19:39:48 +0200

    Added missing ctors, moved non-const Animation::operator{} to public

--- include/cppanim/animation.hpp
+++ include/cppanim/animation.hpp
@@ -15,25 +15,26 @@ namespace cppanim::gfx {
 	using namespace cppanim::fundamentals;
 
 	class Animation : public Drawable {
-		
+
 		std::vector<Frame> data;
 		std::size_t duration;
 		float playRate;
 
-	        Frame& operator[](std::size_t n);
-		
 	public:
 		Animation(XY size, std::size_t duration);
 		Animation(XY size, std::size_t duration, float playRate);
+		Animation(const std::vector<Frame> &data);
+		Animation(const std::vector<Frame> &data, float playRate);
 		Animation(const Animation& copy);
 
 		const std::size_t& getDuration() const;
 
 		// Gets Frame at @p t.
 		const Frame& operator[](std::size_t t) const;
+		Frame& operator[](std::size_t t);
 		// Gets Frame at @p t with temporal transformation
-		const Frame& operator()(std::size_t t) const;		
-		
+		const Frame& operator()(std::size_t t) const;
+
 		_cppanim_getterDecl(float, getPlayRate);
 		void setPlayRate(float p);
 
--- src/animation.cpp
+++ src/animation.cpp
@@ -4,6 +4,33 @@
 
 namespace cppanim::gfx {
 
+        Animation::Animation(XY size, std::size_t duration)
+                             : duration(duration), data(duration, Frame(size))
+                             , playRate(1)
+        {
+                data.reserve(duration);
+        }
+
+        Animation::Animation(XY size, std::size_t duration, float playRate)
+                             : duration(duration), data(duration, Frame(size))
+                             , playRate(playRate)
+        {
+                data.reserve(duration);
+        }
+
+        Animation::Animation(const Animation& copy) : duration(copy.duration)
+                                                      , data(copy.data),
+                                                      playRate(copy.playRate)
+                                                      {}
+
+        Animation::Animation(const std::vector<Frame> &data)
+                                        : duration(data.size()), data(data),
+                                          playRate(1) {}
+
+        Animation::Animation(const std::vector<Frame> &data, float playRate)
+                                        : duration(data.size()), data(data),
+                                          playRate(playRate) {}
+
 	const std::size_t& Animation::getDuration() const { return duration;}
 	_cppanim_getterDefin(Animation, float, playRate, getPlayRate);
 	void Animation::setPlayRate(float p) { playRate = p; }
@@ -16,12 +43,12 @@ namespace cppanim::gfx {
 
 	const Frame& Animation::operator()(std::size_t t) const
 	{ return (*this)[ floor( fmod( t, duration/playRate ) ) ]; }
-	
+
         std::tuple<const Frame&, const XY&> Animation::draw(const Context& c)
 	{
 		return std::tuple<const Frame&, const XY&>(
 			(*this)(c.globalClock),
 		        size);
 	}
-	
+
 }
