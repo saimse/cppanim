59e66c1657f64d75ea52a3e5d90a7dcc6e509547
idzaaa9 <47331937+idzaaa9@users.noreply.github.com>
2020-05-16 14:06:42 +0200

    implemented input handling for WIN32

--- src/screen.cpp
+++ src/screen.cpp
@@ -10,8 +10,84 @@ namespace cppanim::gfx {
 		objects.push_back(&d);
 		std::sort(objects.begin(), objects.end());
 	}
-	
-#ifndef _WIN32
+
+#ifdef _WIN32
+#define S(m)					\
+	e.isSpecialKey = true;			\
+	e.specialKey = KeyboardEvent::m;	\
+	break;
+
+	void Screen::handleInput()
+	{
+		KeyboardEvent e;
+
+		unsigned char c = getch_();
+		if(c == 224 || c == 0){
+			c = getch_();
+			switch(c){
+			case 72:
+				S(E_KEYBOARD_ARR_UP)
+			case 75:
+				S(E_KEYBOARD_ARR_LEFT)
+			case 77:
+				S(E_KEYBOARD_ARR_RIGHT)
+			case 80:
+				S(E_KEYBOARD_ARR_DOWN)
+			case 59:
+				S(E_KEYBOARD_F1)
+			case 60:
+				S(E_KEYBOARD_F2)
+			case 61:
+				S(E_KEYBOARD_F3)
+			case 62:
+				S(E_KEYBOARD_F4)
+			case 63:
+				S(E_KEYBOARD_F5)
+			case 64:
+				S(E_KEYBOARD_F6)
+			case 65:
+				S(E_KEYBOARD_F7)
+			case 66:
+				S(E_KEYBOARD_F8)
+			case 67:
+				S(E_KEYBOARD_F9)
+			case 68:
+				S(E_KEYBOARD_F10)
+			case 133:
+				S(E_KEYBOARD_F11)
+			case 134:
+				S(E_KEYBOARD_F12)
+			case 71:
+				S(E_KEYBOARD_HOME)
+			case 79:
+				S(E_KEYBOARD_END)
+			case 82:
+				S(E_KEYBOARD_INS)
+			case 83:
+				S(E_KEYBOARD_DEL)
+			case 73:
+				S(E_KEYBOARD_PGUP)
+			case 81:
+				S(E_KEYBOARD_PGDN)
+			default:
+				return;
+			}
+		} else if(c >= 1 && c <= 26) {
+			e.setState(KeyboardEvent::E_KEYBOARD_CONTROL);
+			e.data = 'A' + c - 1;
+		} else if((c >= 32 && c < 127)
+			  ||c == '\t' || c == '\b' || c == '\n'){
+			//127 is DEL which is handled above
+			e.data = c;
+		}
+
+		for(auto l : listeners){
+			l->onEvent(e);
+		}
+	}
+#undef S
+
+#else
 
 #include <string.h>
 #include <stdlib.h>
@@ -45,16 +121,16 @@ namespace cppanim::gfx {
 		}
 
 		char buf[5] = {};
-	
+
 		if(n == 3) {
 			fread(buf, sizeof(char), 3, stdin);
-			o(buf);	
-		
+			o(buf);
+
 			//TODO: fix this
 			for(auto l : listeners) {
 				l->onEvent(e);
 			}
-			
+
 		} else if(n == 4) {
 			fread(buf, sizeof(char), 4, stdin);
 
@@ -68,14 +144,14 @@ namespace cppanim::gfx {
 				for(auto l : listeners) {
 					l->onEvent(e);
 				}
-				
+
 			} else {
 				//TODO: handle each char individually
 			}
 		}
 
 #undef o
-		
+
 		char c = getch_();
 
 		if(c >= 'A' && c <= 'Z') {
@@ -84,7 +160,7 @@ namespace cppanim::gfx {
 		} else if (c >= ' ' && c < 127) {
 			e.data = c;
 		} else if (c == 8 || c == 9 || c == '\n') {
-		        e.isSpecialKey = true;
+			e.isSpecialKey = true;
 			//TODO: fix switch
 			switch(c) {
 			case 8:
@@ -107,5 +183,4 @@ namespace cppanim::gfx {
 		}
 	}
 #endif
-	
 }
