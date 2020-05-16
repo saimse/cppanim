bf4a616cfcff4493da236a45582d4ad5a355d8cf
idzaaa9 <47331937+idzaaa9@users.noreply.github.com>
2020-05-16 15:12:22 +0200

    Created function stub for Linux Screen::handleInput

--- src/screen.cpp
+++ src/screen.cpp
@@ -79,6 +79,8 @@ namespace cppanim::gfx {
 			  ||c == '\t' || c == '\b' || c == '\n'){
 			//127 is DEL which is handled above
 			e.data = c;
+		} else{
+			return;
 		}
 
 		for(auto l : listeners){
@@ -95,87 +97,13 @@ namespace cppanim::gfx {
 	void Screen::handleInput()
 	{
 		KeyboardEvent e;
-
-		int n;
-		ioctl(0, FIONREAD, &n);
-
-#define o(buf)								\
-		if(!strcmp(buf, "\x1B[A")) {				\
-			e.isSpecialKey = true;				\
-			e.specialKey					\
-				= KeyboardEvent::E_KEYBOARD_ARR_UP;	\
-		} else if(!strcmp(buf, "\x1B[B")) {			\
-			e.isSpecialKey = true;				\
-			e.specialKey					\
-				= KeyboardEvent::E_KEYBOARD_ARR_DOWN;	\
-		} else if(!strcmp(buf, "\x1B[C")) {			\
-			e.isSpecialKey = true;				\
-			e.specialKey					\
-				= KeyboardEvent::E_KEYBOARD_ARR_RIGHT;	\
-		} else if(!strcmp(buf, "\x1B[D")) {			\
-			e.isSpecialKey = true;				\
-			e.specialKey					\
-				= KeyboardEvent::E_KEYBOARD_ARR_LEFT;	\
-		} else {						\
-			/* TODO: handle each char individually*/	\
-		}
-
-		char buf[5] = {};
-
-		if(n == 3) {
-			fread(buf, sizeof(char), 3, stdin);
-			o(buf);
-
-			//TODO: fix this
-			for(auto l : listeners) {
-				l->onEvent(e);
-			}
-
-		} else if(n == 4) {
-			fread(buf, sizeof(char), 4, stdin);
-
-			if(buf[0] == buf[1] && buf[0] == '\x1B') {
-
-				e.setState(KeyboardEvent::E_KEYBOARD_META);
-				memmove(buf, buf+1, 3);
-				o(buf);
-
-				//TODO: fix this
-				for(auto l : listeners) {
-					l->onEvent(e);
-				}
-
-			} else {
-				//TODO: handle each char individually
-			}
-		}
-
-#undef o
-
 		char c = getch_();
-
-		if(c >= 'A' && c <= 'Z') {
-			e.setState(KeyboardEvent::E_KEYBOARD_SHIFT);
+		if((c >= 32 && c < 127)
+		   ||c == '\t' || c == '\b' || c == '\n'){
+			//127 is DEL which is handled above
 			e.data = c;
-		} else if (c >= ' ' && c < 127) {
-			e.data = c;
-		} else if (c == 8 || c == 9 || c == '\n') {
-			e.isSpecialKey = true;
-			//TODO: fix switch
-			switch(c) {
-			case 8:
-				e.specialKey
-					= KeyboardEvent::E_KEYBOARD_BSPACE;
-				break;
-			case 9:
-				e.specialKey = KeyboardEvent::E_KEYBOARD_TAB;
-				break;
-			case '\n':
-				e.specialKey
-					= KeyboardEvent::E_KEYBOARD_ENTER;
-				break;
-			}
-			e.data = c;
+		} else{
+			return;
 		}
 
 		for(auto l : listeners) {
