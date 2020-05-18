9b67da23c41b3522941597fd4d9d3731209b0a87
Mile <vuletic.mile@jjzmaj.edu.rs>
2020-05-18 17:41:02 +0200

    Implemented semi-working ANSI terminal code parser

--- src/screen.cpp
+++ src/screen.cpp
@@ -3,6 +3,14 @@
 #include <algorithm>
 #include <stdio.h>
 
+#ifndef _WIN32
+#include <map>
+#include <regex>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#endif
+
 namespace cppanim::gfx {
 
 	void Screen::addDrawable(const Drawable &d)
@@ -91,24 +99,131 @@ namespace cppanim::gfx {
 
 #else
 
-#include <string.h>
-#include <stdlib.h>
+	static const constexpr int bufsize = 8;
+
+	static std::tuple<KeyboardEvent, int> parseOne(char buf[bufsize])
+	{
+		KeyboardEvent e;
+		int parsed = 0;
+
+		const std::map<std::string, KeyboardEvent::SpecialKey> map{
+			{"^\033\\[A",   KeyboardEvent::E_KEYBOARD_ARR_UP},
+			{"^\033\\[B",   KeyboardEvent::E_KEYBOARD_ARR_DOWN},
+			{"^\033\\[C",   KeyboardEvent::E_KEYBOARD_ARR_RIGHT},
+			{"^\033\\[D",   KeyboardEvent::E_KEYBOARD_ARR_LEFT},
+			{"^\033OP",     KeyboardEvent::E_KEYBOARD_F1},
+			{"^\033OQ",     KeyboardEvent::E_KEYBOARD_F2},
+			{"^\033OR",     KeyboardEvent::E_KEYBOARD_F3},
+			{"^\033OS",     KeyboardEvent::E_KEYBOARD_F4},
+			{"^\033\\[15", KeyboardEvent::E_KEYBOARD_F5},
+			{"^\033\\[17", KeyboardEvent::E_KEYBOARD_F6},
+			{"^\033\\[18", KeyboardEvent::E_KEYBOARD_F7},
+			{"^\033\\[19", KeyboardEvent::E_KEYBOARD_F8},
+			{"^\033\\[20", KeyboardEvent::E_KEYBOARD_F9},
+			{"^\033\\[21", KeyboardEvent::E_KEYBOARD_F10},
+			{"^\033\\[23", KeyboardEvent::E_KEYBOARD_F11},
+			{"^\033\\[24", KeyboardEvent::E_KEYBOARD_F12},
+			{"^\033\\[1",  KeyboardEvent::E_KEYBOARD_HOME},
+			{"^\033\\[2",  KeyboardEvent::E_KEYBOARD_INS},
+			{"^\033\\[4",  KeyboardEvent::E_KEYBOARD_END},
+			{"^\033\\[5",  KeyboardEvent::E_KEYBOARD_PGUP},
+			{"^\033\\[6",  KeyboardEvent::E_KEYBOARD_PGDN},
+		};
+
+		for(auto it = map.begin(); it != map.end(); it++) {
+			std::cmatch m;
+			if(std::regex_search(buf+parsed, m,
+					std::basic_regex<char>(it->first))) {
+				e.isSpecialKey = true;
+				e.specialKey = it->second;
+				parsed += m.length();
+
+
+				if(std::regex_match(buf + m.length(),
+					 std::basic_regex<char>(";2(F|H)"))){
+					e.setState(KeyboardEvent::E_KEYBOARD_SHIFT);
+					parsed += 3;
+				}
+
+				else if(std::regex_match(buf + m.length(),
+					 std::basic_regex<char>(";5(F|H)"))){
+					e.setState(KeyboardEvent::E_KEYBOARD_CONTROL);
+					parsed += 3;
+				}
+
+				return {e, parsed};
+			}
+		}
+
+		if(buf[0] == '\033' && buf[1] != '[') {
+			e.setState(KeyboardEvent::E_KEYBOARD_META);
+			parsed++;
+		}
+
+		if(buf[parsed] >= 1 && buf[parsed] <= 26) {
+			e.setState(KeyboardEvent::E_KEYBOARD_CONTROL);
+			e.data = 'A' + buf[parsed] - 1;
+			parsed++;
+		} else if((buf[parsed] >= 32 && buf[parsed] < 127)
+			  || buf[parsed] == '\t'|| buf[parsed] == '\b'
+			  || buf[parsed] == '\n'){
+			e.data = buf[parsed];
+			parsed++;
+		}
+
+		return {e, parsed};
+	}
+
+	static struct termios old, current;
+	static inline void initTermios(int echo)
+	{
+		tcgetattr(0, &old);
+		current = old;
+		current.c_lflag &= ~ICANON; /* disable buffered i/o */
+		if (echo) {
+			current.c_lflag |= ECHO; /* set echo mode */
+		} else {
+			current.c_lflag &= ~ECHO; /* set no echo mode */
+		}
+		tcsetattr(0, TCSANOW, &current);
+	}
+
+	static inline void resetTermios(void){ tcsetattr(0, TCSANOW, &old); }
+
 
 	void Screen::handleInput()
 	{
-		KeyboardEvent e;
-		char c = getch_();
-		if((c >= 32 && c < 127)
-		   ||c == '\t' || c == '\b' || c == '\n'){
-			//127 is DEL which is handled above
-			e.data = c;
-		} else{
-			return;
+		struct {char buf[bufsize]; int size;} inbuf = {{}, 0};
+
+		// This disables echoing and turns off canonical mode.
+		// While in non-canonical mode, read(2) will be non-blocking
+		initTermios(0);
+
+		inbuf.size = read(0, inbuf.buf, bufsize - inbuf.size);
+
+		// Buffer might have more than one inputted character,
+		// so, reparse until you fail. Then, flush.
+		while(inbuf.size > 0) {
+			auto parse = parseOne(inbuf.buf);
+
+			KeyboardEvent e = std::get<0>(parse);
+			int parsed = std::get<1>(parse);
+
+			if(parsed == 0) {
+				memmove(inbuf.buf, inbuf.buf+1, bufsize - 1);
+				inbuf.size -= parsed;
+			} else {
+				for(auto l : listeners) {
+					l->onEvent(e);
+				}
+				memmove(inbuf.buf, inbuf.buf + parsed,
+					bufsize - parsed);
+				inbuf.size -= parsed;
+			}
 		}
 
-		for(auto l : listeners) {
-			l->onEvent(e);
-		}
+		resetTermios();
+
 	}
 #endif
 }
