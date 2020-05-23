d9dc8d698f6139e4ca6c068686fef0ebc8117de4
Mile <vuletic.mile@jjzmaj.edu.rs>
2020-05-23 21:46:04 +0200

    Added comments in example demo

--- utils/demos/singleAnimation.cpp
+++ utils/demos/singleAnimation.cpp
@@ -1,18 +1,43 @@
 #include <cppanim.hpp>
-#include <signal.h>
-
-void handle(int a)
-{
-	printf("%d\n", a);
-}
 
 using namespace cppanim::gfx;
 using namespace cppanim::util;
 
+class MoveWithArrows : public Animation, public KeyboardListener {
+public:
+	MoveWithArrows() = delete;
+	explicit MoveWithArrows(const Animation& a) : Animation(a)
+	{
+		// Register this class for listening to keypresses
+		Screen::getInstance().registerListener(*this);
+	}
+
+	virtual void onEvent(const KeyboardEvent& e) override
+	{
+		// Arrow keys are special keys, so isSpecialKey is set
+		// For a full list of special keys, see cppanim/event.hpp
+		if(e.isSpecialKey) {
+			if(e.specialKey == KeyboardEvent::E_KEYBOARD_ARR_UP) {
+				if(position.y > 0) position.y--;
+			} else if(e.specialKey == KeyboardEvent::E_KEYBOARD_ARR_DOWN) {
+				if(position.y < getWindowSize().y + size.y) position.y++;
+			} else if(e.specialKey == KeyboardEvent::E_KEYBOARD_ARR_LEFT) {
+				if(position.x > 0) position.x--;
+			} else if(e.specialKey == KeyboardEvent::E_KEYBOARD_ARR_RIGHT) {
+				if(position.x < getWindowSize().x + size.x) position.x++;
+			}
+		}
+	}
+};
+
 int main()
 {
 	Screen &s = Screen::getInstance();
-	Animation a {
+
+	// MoveWithArrows inherits Animation, and takes a reference to one
+	// in its ctor, so as to copy it over. Animation and underlying classes
+	// can be implicitly constructed with brace initializers
+	MoveWithArrows a ({
 		{3, 3},
 		{
 			{'O', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
@@ -25,12 +50,19 @@ int main()
 			{' ', ' ', ' ', ' ', ' ', ' ', ' ', 'O', ' '},
 			{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'O'},
 		},
-	};
+	});
+	// Add the object to the screen
 	s.addDrawable(a);
+
+	// Start drawing. This will start two new threads.
+	// Using std{in,out} before a call to stop() or wait()
+	// is ill-advised.
 	s.start();
 
+	// Sleep in the main thread
 	cppanim::fundamentals::sleep(1000000);
 
+	// Join the drawing threads
 	s.wait();
 	return 0;
 }
