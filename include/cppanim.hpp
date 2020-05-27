d4a712b862a29d2b28f12cf7bdd93e0647877747
Mile <vuletic.mile@jjzmaj.edu.rs>
2020-05-27 18:57:51 +0200

    Implemented mixins; mixin demo

--- include/cppanim.hpp
+++ include/cppanim.hpp
@@ -4,6 +4,7 @@
 #include <cppanim/animation.hpp>
 #include <cppanim/context.hpp>
 #include <cppanim/drawable.hpp>
+#include <cppanim/mixin.hpp>
 #include <cppanim/frame.hpp>
 #include <cppanim/fundamentals.hpp>
 #include <cppanim/screen.hpp>
